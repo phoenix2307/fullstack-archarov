707f0ae53d44e72ae49ab80b5b177ef3a6cf675c
Alex <phoenix.trade2307@gmail.com>
2025-08-20 21:29:38 +0300

    jwt

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello Lex and Nika!');
 })
+app.post('/auth/login', (req, res) => {
 
-app.listen(4444, (err)=>{
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'Lex UA'
+        }, 'secret2307',
+    )
+
+    console.log(req.body);
+    res.json({
+        success: true,
+        token,
+    })
+})
+
+app.listen(4444, (err) => {
     if (err) {
         return console.log(err);
     }
